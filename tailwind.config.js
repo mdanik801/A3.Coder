b28a833346963db6d01843dd3cc3f9e4f6080deb
Md Aulad Hossain Anik <122743454+mdanik801@users.noreply.github.com>
2024-07-02 20:32:47 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 import keepPreset from "keep-react/src/keep-preset.js";
 
+import resolve from "@rollup/plugin-node-resolve";
+
 export default {
    content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
       "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
    ],
+
    presets: [keepPreset],
    theme: {
       extend: {
@@ -18,5 +21,9 @@ export default {
          },
       },
    },
-   plugins: [],
+   plugins: [
+      resolve({
+         extensions: [".js", ".jsx"],
+      }),
+   ],
 };
